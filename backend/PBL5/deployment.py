46868de8dc61324dc3f490332646ba1836653d7e
TuanVu219 <supersaza219@gmail.com>
2025-05-07 00:10:00 +0700

    third commit

--- backend/PBL5/deployment.py
+++ backend/PBL5/deployment.py
@@ -8,7 +8,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://' + os.environ['WEBSITE_HOSTNAME'],
 ]
 DEBUG = False
-
+SECRET_KEY = os.environ['SECRET']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise middleware
