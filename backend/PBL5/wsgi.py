63cf6a02fc9bb95a3fbaad4244cb5af567e2f0e8
TuanVu219 <supersaza219@gmail.com>
2025-05-06 23:37:52 +0700

    first commit

---
+++ backend/PBL5/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for PBL5 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+setting_module = 'PBL5.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'PBL5.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PBL5.settings', setting_module)
+
+application = get_wsgi_application()
