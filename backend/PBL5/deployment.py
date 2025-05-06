a66b5e309907aec8b5a3d1f87832be13bab9a9ba
TuanVu219 <supersaza219@gmail.com>
2025-05-07 00:04:30 +0700

    second commit

--- backend/PBL5/deployment.py
+++ backend/PBL5/deployment.py
@@ -1,36 +1,60 @@
+# deployment.py
 import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS=[os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS=[
-    'https://'+os.environ['WEBSITE_HOSTNAME'],
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = [
+    'https://' + os.environ['WEBSITE_HOSTNAME'],
 ]
-DEBUG=False
+DEBUG = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # Better positioning for CORS
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',  
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Fix the connection string parsing
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split('')}
-DATABASES={
+parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': parameters['dbname'],
         'HOST': parameters['host'],
         'USER': parameters['user'],
         'PASSWORD': parameters['password'],
-  
     }
 }
+
+# wsgi.py
+"""
+WSGI config for PBL5 project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Correct the settings module selection
+setting_module = 'PBL5.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'PBL5.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_module)
+
+application = get_wsgi_application()
+
+# .production
+# [config]
+# SCM_DO_BUILD_DURING_DEPLOYMENT=true
--- backend/PBL5/wsgi.py
+++ backend/PBL5/wsgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 setting_module = 'PBL5.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'PBL5.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PBL5.settings', setting_module)
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', setting_module)
 application = get_wsgi_application()
